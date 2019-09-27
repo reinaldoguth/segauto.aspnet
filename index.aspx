be02d0b45d8fa9c977c80147b0bea45bd329d0fb
Reinaldo Guth <54216927+reinaldoguth@users.noreply.github.com>
2019-09-27 17:25:12 -0300

    CRUD usando C# Asp.Net, WebForm e MySQL
    Exemplo de CRUD usando C# Asp.Net, WebForm e MySQL

---
+++ index.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SegAuto_CRUD_MySQL.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:HiddenField ID="hfApoliceID" runat="server" />
+        <table>
+            <tr>
+                <td>
+                    <asp:Label Text="Número da apólice" runat="server" />
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtNrApolice" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label Text="CPF/CNPJ do Segurado" runat="server" />
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtDocSegurado" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label Text="Placa do Veículo" runat="server" />
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtPlacaVeiculo" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label Text="Valor Prêmio" runat="server" />
+                </td>
+                <td colspan="2">
+                    <asp:TextBox ID="txtValorPremio" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td  colspan="2">
+                    <asp:Button Text="Salvar" ID="btnSave" runat="server" OnClick="btnSave_Click" />
+                    <asp:Button Text="Apagar" ID="btnDelete" runat="server" OnClick="btnDelete_Click" />
+                    <asp:Button Text="Limpar" ID="btnClear" runat="server" OnClick="btnClear_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td  colspan="2">
+                    <asp:Label Text="" ID="lblSuccessMessage" runat="server" ForeColor="Green" />
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td  colspan="2">
+                    <asp:Label Text="" ID="lblErrorMessage" runat="server" ForeColor="Red" />
+                </td>
+            </tr>
+        </table>
+        <br/>
+        <asp:GridView ID="gvApolices" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="tb10_nr" HeaderText="Apólice" />
+                <asp:BoundField DataField="tb10_cpfcnpj" HeaderText="CPF/CNPJ" />
+                <asp:BoundField DataField="tb10_placa" HeaderText="Placa" />
+                <asp:BoundField DataField="tb10_valor_premio" HeaderText="Valor Prêmio" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton Text="Selecione" ID="lnkSelect" CommandArgument='<%# Eval("tb10_id") %>' runat="server" OnClick="lnkSelect_OnClick" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace SegAuto_CRUD_MySQL
+{
+    public partial class index : System.Web.UI.Page
+    {
+        string connectionString = @"Server=rwplus.com.br;Database=rwplusco_segauto;Uid=rwplusco_segauto;Pwd=.segauto@;";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Clear();
+                GridFill();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection sqlConn = new MySqlConnection(connectionString))
+                {
+                    sqlConn.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("RegInsertOrUpdate", sqlConn);
+                    sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("_tb10_id", Convert.ToInt32(hfApoliceID.Value == "" ? "0" : hfApoliceID.Value));
+                    sqlCmd.Parameters.AddWithValue("_tb10_nr", txtNrApolice.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_tb10_cpfcnpj", txtDocSegurado.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_tb10_placa", txtPlacaVeiculo.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("_tb10_valor_premio", Convert.ToDecimal(txtValorPremio.Text.Trim()));
+                    sqlCmd.ExecuteNonQuery();
+                    GridFill();
+                    Clear();
+                    lblSuccessMessage.Text = "Registro gravado com sucesso.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        void Clear()
+        {
+            hfApoliceID.Value = "";
+            txtNrApolice.Text = 
+                txtDocSegurado.Text = 
+                txtPlacaVeiculo.Text = 
+                txtValorPremio.Text = "";
+            btnSave.Text = "Salvar";
+            btnDelete.Enabled = false;
+            lblErrorMessage.Text = 
+                lblSuccessMessage.Text = "";
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection sqlConn = new MySqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("RegViewAll", sqlConn);
+                sqlDa.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+                gvApolices.DataSource = dtbl;
+                gvApolices.DataBind();
+            }
+        }
+
+        protected void lnkSelect_OnClick(object sender, EventArgs e)
+        {
+//            int regID = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            int regID = int.Parse((sender as LinkButton).CommandArgument);
+            using (MySqlConnection sqlConn = new MySqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("RegViewById", sqlConn);
+                sqlDa.SelectCommand.Parameters.AddWithValue("_tb10_id", regID);
+                sqlDa.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable dtbl = new DataTable();
+                sqlDa.Fill(dtbl);
+
+                txtNrApolice.Text = dtbl.Rows[0][1].ToString();
+                txtDocSegurado.Text = dtbl.Rows[0][2].ToString();
+                txtPlacaVeiculo.Text = dtbl.Rows[0][3].ToString();
+                txtValorPremio.Text = dtbl.Rows[0][4].ToString();
+
+                hfApoliceID.Value = dtbl.Rows[0][0].ToString();
+
+                btnSave.Text = "Atualizar";
+                btnDelete.Enabled = true;            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConn = new MySqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                MySqlCommand sqlCmd = new MySqlCommand("RegDeleteById", sqlConn);
+                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("_tb10_id", Convert.ToInt32(hfApoliceID.Value == "" ? "0" : hfApoliceID.Value));
+                sqlCmd.ExecuteNonQuery();
+                GridFill();
+                Clear();
+                lblSuccessMessage.Text = "Registro apagado com sucesso.";
+            }
+        }
+    }
+}
